d50b5486a2ca1c1477ccbb73cb897dc212a09087
Shubham Gadekar <shubham.gadekar.alpha@gmail.com>
2025-08-28 20:32:24 +0530

    Added app exit dialog on Home page.

--- app/src/main/java/com/alpha/myeyecare/presentation/ui/home/HomeScreen.kt
+++ app/src/main/java/com/alpha/myeyecare/presentation/ui/home/HomeScreen.kt
@@ -1,5 +1,8 @@
 package com.alpha.myeyecare.presentation.ui.home
 
+import android.annotation.SuppressLint
+import android.app.Activity
+import androidx.activity.compose.BackHandler
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
@@ -10,6 +13,8 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.RateReview
 import androidx.compose.material.icons.filled.WaterDrop
 import androidx.compose.material.icons.rounded.Visibility
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Button
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ExtendedFloatingActionButton
@@ -20,17 +25,21 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import com.alpha.myeyecare.common.constants.AppDestinations
 import com.alpha.myeyecare.presentation.ui.CheckUserNotificationPermission
 import com.alpha.myeyecare.presentation.ui.common.FeatureCard
-import com.google.accompanist.permissions.ExperimentalPermissionsApi
-import com.google.accompanist.permissions.PermissionState
 
+@SuppressLint("ContextCastToActivity")
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun HomeScreen(
@@ -40,6 +49,25 @@ fun HomeScreen(
     onGoToSuggestionsClicked: () -> Unit,
     updatePermissionStatus: () -> Unit
 ) {
+
+    var showExitDialog by remember { mutableStateOf(false) }
+    val activity = (LocalContext.current as? Activity)
+
+    BackHandler(enabled = true) {
+        showExitDialog = true
+    }
+
+    if (showExitDialog) {
+        ExitConfirmationDialog(
+            onConfirmExit = {
+                showExitDialog = false
+                activity?.finish()
+            },
+            onDismiss = {
+                showExitDialog = false
+            }
+        )
+    }
 
     CheckUserNotificationPermission(
         fromReminderDetailsScreen = false,
@@ -126,4 +154,26 @@ fun HomeScreen(
             )
         }
     }
+}
+
+@Composable
+fun ExitConfirmationDialog(
+    onConfirmExit: () -> Unit,
+    onDismiss: () -> Unit
+) {
+    AlertDialog(
+        onDismissRequest = onDismiss,
+        title = { Text("Confirm Exit") },
+        text = { Text("Are you sure you want to exit the app?") },
+        confirmButton = {
+            Button(onClick = onConfirmExit) {
+                Text("Exit")
+            }
+        },
+        dismissButton = {
+            Button(onClick = onDismiss) {
+                Text("Cancel")
+            }
+        }
+    )
 }
